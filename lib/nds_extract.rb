dc32252c2be7a61be4a03a04a0a4316c102c986c
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-07 20:37:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,35 +34,32 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  arr = []
+  i = 0 
+  
+  while i < movies_collection.length do 
+    arr[i] = movie_with_director_name(name, movies_collection[i])
+    i += 1
+  end
+  arr
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  my_hash = {}
+  i = 0 
+  
+  while i < collection.length do 
+    film = collection[i]
+    
+    if !my_hash[film[:studio]]
+      my_hash[film[:studio]] = film[:worldwide_gross]
+    else 
+      my_hash[film[:studio]] += film[:worldwide_gross]
+    end
+    i += 1 
+  end 
+    my_hash
 end
 
 def movies_with_directors_set(source)
@@ -76,6 +73,16 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  arr = []
+  i = 0 
+    while i < source.length
+      dir_name = source[i][:name]
+      arr.push(dir_name)
+      i += 1
+    end
+    puts arr
+  
+  
 end
 
 # ----------------    End of Your Code Region --------------------
