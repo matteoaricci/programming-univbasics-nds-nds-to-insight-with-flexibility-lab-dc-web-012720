a7dd53c19cf97d5517d599252dea95c28c50357e
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-07 20:49:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -73,15 +73,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  arr = []
-  i = 0 
-  j = 0
-    while i < source.length
-      dir_name = source[i][:name]
-      arr.push(dir_name)
-      i += 1
-    end
-    puts arr
+  
   
   
 end
